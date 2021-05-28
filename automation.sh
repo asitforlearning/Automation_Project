e12b1d8ccfde1a2ab98b159568157ac75f29e409
Asit Kumar <asitforlearning@gmail.com>
2021-05-28 19:54:08 +0530

    Adding task3 changes to automation.sh for cron job and audit logs

--- automation.sh
+++ automation.sh
@@ -10,6 +10,8 @@ myname='asit'
 timestamp=`date '+%d%m%Y-%H%M%S'`
 ##log path to track execution details of this script
 automation_log=/tmp/automation_script_log.txt
+inventory_file=/var/www/html/inventory.html
+cron_entry="1 0 * * *	root	/root/Automation_Project/automation.sh"
 #variables to initialize END
 
 
@@ -63,6 +65,17 @@ else
 	exit_on_fail $status " service apache2 start"
 fi
 
+##changes for Version 2  / task 3 START checking/initializing inventory_file
+if [[ -f $inventory_file ]]
+then
+	echo "${timestamp}: $inventory_file is present" >>$automation_log
+else
+	echo "Log Type			Time Created			Type			Size">>$inventory_file
+	status=$?
+	exit_on_fail $status " creation of $inventory_file"
+fi
+##changes for Version 2  / task 3 FINISH checking/initializing  inventory_file
+
 ##creating tar for log files
 archive_name=/tmp/${myname}-httpd-logs-${timestamp}.tar
 tar cvf  $archive_name --wildcards -C /var/log/apache2/  access.log error.log
@@ -71,6 +84,10 @@ tar cvf  $archive_name --wildcards -C /var/log/apache2/  access.log error.log
 ## log archive copy to s3 bucket  
 if [[ -f  $archive_name ]] 
 then
+	##changes for Version 2  / task 3 START  writing archive status
+	file_size=`du -h $archive_name|cut -f1`
+	echo "httpd-logs			${timestamp}			tar			$file_size">>$inventory_file
+	##changes for Version 2  / task 3 END  writing archive status
 	aws s3 \
 	cp /tmp/${myname}-httpd-logs-${timestamp}.tar \
 	s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
@@ -79,6 +96,35 @@ then
 else
 	echo "logs archive file $archive_name  was not available." >>$automation_log
 fi	
-	
+
+
+##changes for Version 2  / task 3 START  creating crontab entry
+jobs_temp_file=\tmp\cronf_file
+#store existing jobs to cron file
+crontab -l>${jobs_temp_file}
+cron_file_exists=$?
+
+if [[ $cron_file_exists -eq 0 ]] 
+then
+	echo "${timestamp}: cron jobs file exists for user" >>$automation_log
+else
+	##remove verbose text
+	echo "">${jobs_temp_file}
+fi
+
+grep '/root/Automation_Project/automation.sh' ${jobs_temp_file}
+cron_automation_entry_exists=$?
+
+if [[ $cron_automation_entry_exists -eq 0 ]] 
+then
+	echo "${timestamp}: cron job entry already exists for automation task" >>$automation_log
+else
+	echo "${cron_entry}">>${jobs_temp_file}
+	crontab ${jobs_temp_file}
+	status=$?
+	exit_on_fail $status " cron entry creation for automation task"
+fi
+##changes for Version 2  / task 3 END  creating crontab entry
+
 echo "END run @ $timestamp*******************************">>$automation_log
 
